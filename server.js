e5ce592fc9cfb093bf9372ad3e2d08edf997c170
hattagh73 <>
2022-06-11 01:11:27 +0800

    Reorganise code in server.js by addin' some comments

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const cors = require('cors');
-var bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const { logger } = require('./middleware/log_events');
@@ -8,37 +7,34 @@ const { globalToken } = require('./middleware/global_token');
 const HOST = process.env.SERVER_HOST;
 const PORT = process.env.SERVER_PORT;
 
-// Custom middleware logger
+/* START Custom middleware logger */ 
 app.use(logger);
 app.use(globalToken);
-// Cross Origin Resource Sharing
-// const whitelist = ['http://localhost:3000']
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         if (whitelist.indexOf(origin) !== -1 || !origin) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     optionSuccessStatus: 200,
-//  }
-// app.use(cors(corsOptions));
-app.use(
-    cors({
-        origin: 'http://localhost:3000'
-    })
-);
-// Middleware
+
+const whitelist = ['http://localhost:3000'] // Cross Origin Resource Sharing
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
+
 app.use(express.urlencoded({ extended: false }));
-// will active before all the http method will also supply to all routes
-// kalau takda tak boleh submit form
-app.use(express.json()); 
 
-// Routes
+app.use(express.json());  // For form submition
+/* END */
+
+/* START Routes - User Route */
 app.use("/api/v1/user",  require("./routes/userRoutes"));
+/* END */
 
-// Global Error Handler
+/* START Global Error Handler */ 
 app.use((err, req, res, next) => {
     console.log(err.stack);
     console.log(err.name);
@@ -48,11 +44,14 @@ app.use((err, req, res, next) => {
         message: "Something went rely wrong",
     });
 });
+/* END */ 
 
-// Running Server 
+/* START Run Server */
 app.listen(PORT,HOST, (err) => {
     // If no error 
     if(!err) return console.log(`Server is running. Port: ${PORT} & Host ${HOST}`);
+    
     // If failed shows error
     if (err) return console.log(`Server has failed to start. Add One lah ${err}`);
 });
+/* END */
