930f562653a999cb40fd5f1651b9bd65af8b7fae
hattagh73 <>
2022-06-14 16:51:13 +0800

    Add params variable for user_detail route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.route("/user_list").get(userControllers.getAllUsers);
 router.route("/user_register").post(userControllers.createNewUser);
 router.route("/user_login").post(userControllers.userLogin);
-router.route("/user_detail/").get(userControllers.getUserById);
+router.route("/user_detail/:user_id").get(userControllers.getUserById);
 router.route("/user_update/:user_id").patch(userControllers.updateUserById);
 router.route("/user_delete/").delete(userControllers.deleteUserById);
 
